f1484daedaaa79a47f26027f362e530fb69ec858
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 20:14:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
       SELECT * FROM students WHERE name = ?
     SQL
 
-    row = DB[:conn].execute(sql, name)
+    row = DB[:conn].execute(sql, name).flatten
     self.new_from_db(row)
 
   end
