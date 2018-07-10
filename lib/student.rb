0126963f6b98e5a6fca118cfbe662525cc60ce58
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 20:29:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,8 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    binding.rpy
-    self.first_X_students_in_grade_10(1)
+    binding.pry
+    self.first_X_students_in_grade_10(1).flatten
   end
 
   def self.find_by_name(name)
