932b66d31ba8f90b5819e403685b4708cb791606
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 20:26:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,14 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    rows = DB[:conn].execute(sql, x)
+    rows.collect {|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
