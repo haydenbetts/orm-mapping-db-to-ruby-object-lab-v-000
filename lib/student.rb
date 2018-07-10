8e23d1227ce65626e90725a4e0d113727cec5d33
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 20:12:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(row[1], row[2], row[0])
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
     student
   end
 
